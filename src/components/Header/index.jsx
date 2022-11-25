bf45b72e391ff85722deedad46c478222a8ede07
lucascmpus <lucascmpus@gmail.com>
2022-11-24 21:03:51 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Adicionar",
+    path: "/adicionar",
+  },
+];
+
+function NavAnchor({ path, children }) {
+  return (
+    <Link href={path} legacyBehavior><a>{children}</a></Link>
+  );
+}
+
+export default function Header() {
+  const { pathname } = useRouter();
+
+  return (
+    <header>
+      <nav>
+        <h2>marketlist</h2>
+        <ul>
+          {links.map(({ name, path }) => (
+            <li key={path}>
+              {path === pathname ? (
+                <span>{name}</span>
+              ) : (
+                  <NavAnchor path={path}>{name}</NavAnchor>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
